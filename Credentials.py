fa2829934aa859fe82f2ba95f8e86ac1f7f72768
Jay Marante <Jay.Marante@wdc.com>
2017-11-10 00:00:47 +0800

    Added credentials file; use for debugging

---
+++ Credentials.py
@@ -0,0 +1,13 @@
+class Credentials(object):
+
+    def __init__(self):
+        return
+
+    def get_credentials(self):
+        firstname = "Jay Warren"
+        lastname = "Marante"
+        username = "jaywarren"
+        useremail = "jaythesis@yahoo.com"
+        password = "Hgstinc1*"
+        keyword = "Firmware"
+        return firstname, lastname, username, useremail, password, keyword
